454899a2c6112c6f3db0482941a692b1773c7723
𝖒𝖚𝖘𝖙𝖆𝖋𝖆 <121257754+mustafaakalin@users.noreply.github.com>
2025-01-19 09:45:01 +0300

    Initialize C# project with basic loop examples and .NET 8.0 configuration

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// 1. For loop -- dizi ve koleksiyon işlemleri 
+
+// soru: 1-10 arasındaki sayıları ekrana yazdırınız
+
+ for (int i = 1; i <= 10; i++)
+    {
+        Console.WriteLine(i);
+    }
+// soru 2: 1 10 arasındaki sayıları 2 arttırarak yazdırınız
+
+for (int i = 1; i <= 10; i+=2)
+    {
+        Console.WriteLine(i);
+    }
+
+// 2. While loop - ado.net database işlemleri
+
+// 3. Do-While loop
+
+// 4. Foreach loop -- mvc, .net, core da listeleme işlemlerinde kullanılır
+
+// 5. Nested loop
+
+// 6. Infinite loop
+
+// 7. Loop control statements
+
+// 8. Jump statements
+
+// 9. Looping through arrays
+
+// 10. Looping through strings
+
+// 11. Looping through collections
+
+// 12. Looping through dictionaries
+
+// 13. Looping through lists
+
+// 14. Looping through multidimensional arrays
+
+// 15. Looping through jagged arrays
+
+// 16. Looping through tuples
+
+// 17. Looping through files
+
+// 18. Looping through directories
+
+// 19. Looping through dates
+
+// 20. Looping through numbers
+
+// 21. Looping through characters
+
+// 22. Looping through enums
+
+// 23. Looping through tasks
+
+// 24. Looping through async methods
+
+// 25. Looping through async streams
+
+// 26. Looping through async enumerables
+
+// 27. Looping through async collections
+
+// 28. Looping through async dictionaries
+
+// 29. Looping through async lists
+
+// 30. Looping through async multidimensional arrays
+
+// 31. Looping through async jagged arrays
+
+// 32. Looping through async tuples
+
+// 33. Looping through async files
+
+// 34. Looping through async directories
